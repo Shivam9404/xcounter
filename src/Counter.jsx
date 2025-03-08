f1f18a6bb3872d68c7b17b60b6f81425f6e8671f
Shivam Shinde <shivamshinde94046788@gmail.com>
2025-03-08 10:48:55 +0530

    xcounter-test1

---
+++ src/Counter.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+function Counter (){
+
+    const[count, setCount] = useState('')
+
+    const handleIncrement = () =>{
+        setCount(count+1);
+    };
+
+    const handleDecrement = () =>{
+        setCount(count-1);
+    };
+
+    return(
+        <div>
+            <h1>Counter App</h1>
+            <p>Count: {count}</p>
+            <div>
+                <button onClick={handleIncrement}>Increment</button>
+                <button onClick={handleDecrement}>Decrement</button>
+            </div>
+        </div>
+        
+    )
+};
+
+export default Counter;
